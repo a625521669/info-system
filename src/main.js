91e6d96c816f5e944d23e13f6b224e7b5c5ab630
a625521669 <625521669@qq.com>
2017-02-21 18:01:29 +0800

    normalupdate

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-// import '../theme/index.css'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import vueResource from 'vue-resource'
-
-
 import routerMap from './router.js'
+
+
 
 Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.use(vueResource);
 
 
-const router = new VueRouter({routes: routerMap})
+const router = new VueRouter({
+  mode: 'history',
+  routes: routerMap
+})
 
 const app = new Vue({
   router
